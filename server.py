13481163d5dfdb59b8e5f88d5482d339402ea7bc
Amal K Paul <60135524+paulstarmail@users.noreply.github.com>
2025-04-13 23:12:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 
-# The only argument is the desired port number.
-
 import sys
+import requests
 import subprocess
 from subprocess import check_output
 
+def get_public_ip():
+    try:
+        response = requests.get("https://api.ipify.org?format=json")
+        ip = response.json()["ip"].strip()
+        return ip
+    except requests.RequestException as e:
+        print("Error fetching IP:", e)
+        return None
+
 try:
-	port = str(sys.argv[1])
+    port = str(sys.argv[1])
 except:
-	port = "5001"
+    port = "6004"
 ip = check_output(['hostname', '--all-ip-addresses'])
 ip = ip.decode("utf-8").strip()
-print("URL: http://" + ip + ":" + port)
+print("Public URL: http://" + str(get_public_ip()) + ":30004")
+print("Local URL: http://" + ip + ":" + port)
 try:
-	subprocess.run(["python3", "-m", "http.server", port], check=True)
+    subprocess.run(["python3", "-m", "http.server", port], check=True)
 except subprocess.CalledProcessError:
-	print("\nERROR: Please try giving another port number.")
+    print("\nERROR: The port " + port + " is in use by some other program.")
